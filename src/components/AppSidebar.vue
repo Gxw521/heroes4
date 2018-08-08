be81085faded559eabdda83dfe302f00c2d2d79e
Licheng Xu <jsupgo@163.com>
2018-08-08 11:23:31 +0800

    路由

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,9 +1,9 @@
 <template>
   <ul class="nav nav-sidebar">
     <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-    <li><a href="#">Reports</a></li>
-    <li><a href="#">Analytics</a></li>
-    <li><a href="#">Export</a></li>
+    <li><a href="#">英雄列表</a></li>
+    <li><a href="#">武器列表</a></li>
+    <li><a href="#">装备列表</a></li>
   </ul>
 </template>
 
