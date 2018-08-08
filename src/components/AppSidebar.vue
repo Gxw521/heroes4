d2c5796a2da4433a7d0785244ca0e3dd5634f9c4
Licheng Xu <jsupgo@163.com>
2018-08-08 11:40:30 +0800

    导航

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,9 +1,17 @@
 <template>
   <ul class="nav nav-sidebar">
-    <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-    <li><a href="#">英雄列表</a></li>
+    <!-- <li><a href="#" class="active">英雄列表</a></li>
     <li><a href="#">武器列表</a></li>
-    <li><a href="#">装备列表</a></li>
+    <li><a href="#">装备列表</a></li> -->
+    <router-link tag="li" to="/heroes">
+      <a>英雄列表</a>
+    </router-link>
+    <router-link tag="li" to="/equips">
+      <a>装备列表</a>
+    </router-link>
+    <router-link tag="li" to="/weapons">
+      <a>武器列表</a>
+    </router-link>
   </ul>
 </template>
 
